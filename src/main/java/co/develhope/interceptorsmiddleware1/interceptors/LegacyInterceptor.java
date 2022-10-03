a0aaf872e226f38ed383cf60f455701cf3e449a5
Emanuele Di Gregoli <109800866+ariosto22@users.noreply.github.com>
2022-10-03 17:49:23 +0200

    Add files via upload

---
+++ src/main/java/co/develhope/interceptorsmiddleware1/interceptors/LegacyInterceptor.java
@@ -0,0 +1,27 @@
+package co.develhope.interceptorsmiddleware1.interceptors;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LegacyInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getRequestURL().toString().contains("/legacy")) {
+
+            throw new Exception("This endpoint is blocked");
+        }
+        response.setStatus(410);
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
