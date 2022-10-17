70d910f1dfa44712ed1ce7e11d2b91d30f17bd8a
ariosto22 <emanuele.digregoli40@gmail.com>
2022-10-17 11:26:57 +0200

    API_SpringRepositories01

---
+++ src/main/java/co/develhope/APISpringRepositories01/entities/Car.java
@@ -0,0 +1,26 @@
+package co.develhope.APISpringRepositories01.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String modelName;
+    @Column(nullable = false)
+    private String serialNumber;
+    @Column(nullable = true)
+    private double currentPrice;
+}
