70357b7e086203deab6bde233319cbd13df254f0
ariosto22 <emanuele.digregoli40@gmail.com>
2022-10-31 18:24:38 +0100

    File upload and download - Service Exercise

---
+++ src/main/java/co/develhope/fileUploadAndDownload/FileUploadAndDownloadApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.fileUploadAndDownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadAndDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadAndDownloadApplication.class, args);
+	}
+
+}
